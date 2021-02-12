7a5ac2133866c674afaaf0d7b287edbd4aa0fdb9
tajud99n <giwa208@gmail.com>
2021-02-12 16:40:21 +0100

    feat: base route

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,9 +1,15 @@
 const express = require("express");
-
-const api = require("./v1");
+const { StatusCodes } = require("http-status-codes");
+const http_responder = require("../utils/http_response");
 
 const router = express.Router();
 
-router.use("/api/v1", api);
+router.get("/health", (req, res) => {
+	return http_responder.successResponse(res, null, "Series Server is up & Running", StatusCodes.OK, null);
+});
+
+router.get("/", (req, res) => {
+	return http_responder.successResponse(res, null, null, StatusCodes.OK, null);
+});
 
 module.exports = router;
