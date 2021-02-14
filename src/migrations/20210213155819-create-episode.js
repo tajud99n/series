15753d4095e246780918cbbe70687933460c7221
tajud99n <giwa208@gmail.com>
2021-02-14 16:55:57 +0100

    chore: update model

--- src/migrations/20210213155819-create-episode.js
+++ src/migrations/20210213155819-create-episode.js
@@ -1,34 +1,30 @@
 'use strict';
 module.exports = {
   up: async (queryInterface, Sequelize) => {
-    await queryInterface.createTable('Episodes', {
-      id: {
-        allowNull: false,
-        autoIncrement: true,
-        primaryKey: true,
-        type: Sequelize.INTEGER
-      },
-      name: {
-        type: Sequelize.STRING
-      },
-      releaseDate: {
-        type: Sequelize.DATE
-      },
-      episodeCode: {
-        type: Sequelize.STRING
-      },
-      created: {
-        type: Sequelize.STRING
-      },
-      createdAt: {
-        allowNull: false,
-        type: Sequelize.DATE
-      },
-      updatedAt: {
-        allowNull: false,
-        type: Sequelize.DATE
-      }
-    });
+    await queryInterface.createTable("Episodes", {
+			id: {
+				allowNull: false,
+				autoIncrement: true,
+				primaryKey: true,
+				type: Sequelize.INTEGER,
+			},
+			name: {
+				type: Sequelize.STRING,
+				allowNull: false,
+			},
+			releaseDate: {
+				type: Sequelize.DATE,
+				allowNull: false,
+			},
+			episodeCode: {
+				type: Sequelize.STRING,
+				allowNull: false,
+			},
+			created: {
+				type: Sequelize.DATE,
+				allowNull: false,
+			},
+		});
   },
   down: async (queryInterface, Sequelize) => {
     await queryInterface.dropTable('Episodes');
