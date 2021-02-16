a44b8bce18145a187aee3bf0b7b63dd4c48d446e
tajud99n <giwa208@gmail.com>
2021-02-16 03:25:09 +0100

    base url

--- src/routes/index.js
+++ src/routes/index.js
@@ -8,11 +8,16 @@ const router = express.Router();
 router.use("/api/v1", api);
 
 router.get("/health", (req, res) => {
-	return http_responder.successResponse(res, null, "Series Server is up & Running", StatusCodes.OK, null);
+	return http_responder.successResponse(res, null, "Series Server is up & Running", StatusCodes.OK);
 });
 
 router.get("/", (req, res) => {
-	return http_responder.successResponse(res, null, null, StatusCodes.OK, null);
+	return http_responder.successResponse(
+		res,
+		{ github: "https://github.com/tajud99n/series.git" },
+		"welcome to series service",
+		StatusCodes.OK
+	);
 });
 
 module.exports = router;
